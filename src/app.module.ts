c42b807ca1870ed322862d92dd2c704e3fba1b17
Deracodes1 <ezemeniachidera@gmail.com>
2026-05-01 16:37:08 -0700

    feat: sucessfully created a local postgre db, connected my nest app to it via tyeporm and adequately set up environmental variables to securely boostrap app

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,33 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
 import { APP_GUARD } from '@nestjs/core';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { LoggerMiddleware } from './middlewares/logger/logger.middleware';
 @Module({
-  imports: [ThrottlerModule.forRoot([{ ttl: 60000, limit: 10 }])],
+  imports: [
+    ThrottlerModule.forRoot([{ ttl: 60000, limit: 10 }]),
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DATABASE_HOST'),
+        port: configService.get<number>('DATABASE_PORT'),
+        username: configService.get<string>('DATABASE_USERNAME'),
+        password: configService.get<string>('DATABASE_PASSWORD'),
+        database: configService.get<string>('DATABASE_DATABASE'),
+        autoLoadEntities: true,
+        synchronize: true,
+        extra: {
+          max: 20, // Increase the maximum number of clients in the pool
+          idleTimeoutMillis: 30000,
+          connectionTimeoutMillis: 2000,
+        },
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService, { provide: APP_GUARD, useClass: ThrottlerGuard }],
 })
