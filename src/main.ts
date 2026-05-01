11e70faf923a5467fc1792c9eb6c41a4c3a97b96
Deracodes1 <ezemeniachidera@gmail.com>
2026-05-01 08:59:31 -0700

    feat: added app versionioning for future scalability and backward compatability. and also a /api prefix

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -7,6 +8,12 @@ async function bootstrap() {
   if (process.env.NODE_ENV === 'production') {
     app.enableShutdownHooks();
   }
+
+  // this will enable global versioning in our nest js application.
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: '1',
+  });
 }
 bootstrap().catch((err) => {
   console.error('Error during bootstrap:', err);
