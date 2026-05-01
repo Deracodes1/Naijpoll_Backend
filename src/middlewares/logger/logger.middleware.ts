3a57f0131035e9625fb5084934fbee53128dfbc4
Deracodes1 <ezemeniachidera@gmail.com>
2026-05-01 14:37:00 -0700

    fix: fixed the logger middleware bug

--- src/middlewares/logger/logger.middleware.ts
+++ src/middlewares/logger/logger.middleware.ts
@@ -1,10 +1,10 @@
 import { Injectable, NestMiddleware, Logger } from '@nestjs/common';
-import { Request } from 'express';
+import { Request, Response, NextFunction } from 'express';
 
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
   private logger = new Logger('HTTP');
-  use(req: Request, res: any, next: () => void) {
+  use(req: Request, res: Response, next: NextFunction) {
     const now = new Date();
     const formattedTime = now.toISOString();
     const apiRequestDetails = {
@@ -13,6 +13,14 @@ export class LoggerMiddleware implements NestMiddleware {
       url: req.url,
     };
     this.logger.log(apiRequestDetails);
+    const startTime = Date.now(); // to keep track of the request lifecycle/time
+    res.on('finish', () => {
+      const { statusCode } = res;
+      const duration = Date.now() - startTime;
+      this.logger.log(
+        `${apiRequestDetails.method} ${apiRequestDetails.url} ${statusCode} - ${duration}ms -IP: ${req.ip}`,
+      );
+    });
     next();
   }
 }
