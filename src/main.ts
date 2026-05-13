a5af0212176567ce9e42806c68a501c27e738f6b
Deracodes1 <ezemeniachidera@gmail.com>
2026-05-13 02:18:07 -0700

    feat: added a proxy configuration

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,11 @@ import { AllExceptionsFilter } from './global/http-exception.filter';
 import { TransformInterceptor } from './global/transform.interceptor';
 import helmet from 'helmet';
 import cookieParser from 'cookie-parser';
+import { NestExpressApplication } from '@nestjs/platform-express';
 async function bootstrap() {
   // this will toggle JSON based logging depending on whether the app is running on production or not
   const isProduction = process.env.NODE_ENV === 'production';
-  const app = await NestFactory.create(AppModule, {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
     logger: isProduction
       ? new ConsoleLogger({ json: true })
       : ['debug', 'error', 'fatal', 'log', 'verbose', 'warn'],
@@ -19,6 +20,7 @@ async function bootstrap() {
   }
 
   app.use(helmet());
+  app.getHttpAdapter().getInstance().set('trust proxy', 'loopback, linklocal');
   app.useGlobalFilters(new AllExceptionsFilter());
   app.useGlobalInterceptors(new TransformInterceptor());
 
